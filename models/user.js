9596496e7c045b8eb0b0e2dded8f3cf5d4cb7f98
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2024-01-14 19:19:33 +0700

    membuat file models/user.js dan membuat user Schema

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+Schema = mongoose.Schema;
+
+// User Schema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true, // menambahkan createdAt dan updatedAt
+    collection: 'users'
+}) 
+
+const User = mongoose.model('users', userSchema) 
+
+module.exports = {
+    User
+}
