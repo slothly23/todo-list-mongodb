bfa5e8a8d7731526f91086e0cb672e95b9034374
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2024-01-14 22:55:09 +0700

    membuat fungsi login

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,9 +2,12 @@ const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
 const { User } = require("../models/user")
 
+// const userModel = require("../models/user")
 
+const secret_key = process.env.JWT_TOKEN
 const saltRounds = 10
 
+// register new user
 const register = async (req, res) => {
     try {
         const username = req.body.username
@@ -26,6 +29,36 @@ const register = async (req, res) => {
     }
 }
 
+//user login
+const login = async (req, res) => {
+    try {
+        const username = req.body.username
+        const password = req.body.password
+    
+        const userInDB = await User.findOne({username: req.body.username});
+    
+        if(!userInDB) {
+            return res.status(401).send('Username not found')
+        }
+    
+        //membandingkan hash password dari database dengan text password
+        const isPasswordMatch = await bcrypt.compare(password, userInDB.password)
+        if(isPasswordMatch) {
+            // Create a JWT token with user information
+            const token = jwt.sign(username, secret_key)
+        
+            // Send the token to the user
+            res.json({ token })
+        } else {
+            return res.status(401).send('Wrong password')
+        }
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send({ message: 'Internal server error'})
+    }
+}
+
 module.exports = {
+    login,
     register
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const express = require("express")
+const { register, login } = require("../controllers/auth")
 const app = express()
 
-const authController = require('../controllers/auth')
+// const authController = require('../controllers/auth')
 
 //express.Router() fungsinya ngebantu kita supaya routernya dibuat sesuai kategori
 const user = express.Router()
 
-user.post('/register', authController.register)
+user.post('/register', register)
+user.post('/login', login)
 
 
 module.exports = {
