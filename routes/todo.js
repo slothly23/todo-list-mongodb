b25af7dfea07842451cf5ce43b4ecc9f666029d1
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2024-01-15 18:26:33 +0700

    menambahkan verifyToken pada routes

--- routes/todo.js
+++ routes/todo.js
@@ -1,16 +1,17 @@
 const express = require("express")
-const { addTodo, getTodo, getDetailTodo, updateTodo, deleteTodo, deleteAllTodo } = require("../controllers/todo")
+const { addTodo, getTodo, getDetailTodo, updateTodo, deleteTodo, deleteAllTodo } = require("../controllers/todo");
+const { verifyToken } = require("../middlewares/auth");
 const app = express()
 
 //express.Router() fungsinya ngebantu kita supaya routernya dibuat sesuai kategori
 const todo = express.Router()
 
-todo.post('/todos', addTodo);
-todo.get('/todos', getTodo);
-todo.get('/todos/:id', getDetailTodo);
-todo.patch('/todos/:id', updateTodo);
-todo.delete('/todos/:id', deleteTodo);
-todo.delete('/todos', deleteAllTodo);
+todo.post('/todos', verifyToken, addTodo);
+todo.get('/todos', verifyToken, getTodo);
+todo.get('/todos/:id', verifyToken, getDetailTodo);
+todo.patch('/todos/:id', verifyToken, updateTodo);
+todo.delete('/todos/:id', verifyToken, deleteTodo);
+todo.delete('/todos', verifyToken, deleteAllTodo);
 
 module.exports = {
     todo
